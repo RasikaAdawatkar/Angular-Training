a04d5bd00959d87381e4a248ea50e71114282f66
Tejashree-Kate <Tejashree Kate@XFT000109739002.ads.mckinsey.com>
2015-04-14 19:14:15 +0530

    adding other func example to service

--- RegularRouting/app/scripts/services/data.js
+++ RegularRouting/app/scripts/services/data.js
@@ -1,8 +1,28 @@
 angular.module('regularRoutingApp')
 .service('Data',function ($http){
+
+	//fetch data
 	this.get_data = function(){
 		return $http.get('../data/books.json');
 	}
 
-	return this;
+	//post data
+	this.add = function(){
+		var url = //add post url//
+		var promise =  $http({
+			method:'POST',
+			url:url,
+			data:{}
+		})
+		return promise;
+	}
+
+	this.update = function(){
+		//update code goes here
+	}
+
+	this.delete = function(){
+		//delete request goes here
+	}
+
 })
